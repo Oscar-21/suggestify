2a2415f335f27059486e58bcdefd0d7c88ff7de3
Oscar-21 <austin.conder@outlook.com>
2017-11-06 15:53:19 -0500

    pre aws push

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-class Home extends Component {
+import React, { PureComponent } from 'react';
+class Home extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       queryString: this.props.location.search || '',
-       accessToken: this.props.location.search.slice(14) || '',
-       me: '',
-       artist: '',
-       playlists: '',
+      accessToken: this.props.location.search.slice(14) || '',
+      me: '',
+      artist: '',
+      playlists: '',
     };
   }
 
   componentWillMount() {
-    if (this.state.accessToken !== '') {
+    if (this.props.location.search !== '' ) {
        console.log('profile loaded');
        fetch('https://api.spotify.com/v1/me', {
-         headers: {
-           Authorization: ` Bearer ${this.state.accessToken}`        
-         },
+        headers: {
+          Authorization: ` Bearer ${this.state.accessToken}`        
+        },
        }).then(response => 
           response.json()
         ).then(me => {
@@ -26,21 +26,21 @@ class Home extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps !== this.props) {
-      console.log(`get playlists for ${this.state.me.id}`);
-      fetch(`https://api.spotify.com/v1/me/playlists`, {
-        headers: {
-          Authorization: ` Bearer ${this.state.accessToken}`
-        },
-      }).then(response => 
-        response.json()
-      ).then(playlists => {
-        this.setState({	playlists: playlists }, () => {
-          console.log(this.state.playlists);
-        });
+
+  getMyPlaylists = () => {
+    fetch(`https://api.spotify.com/v1/me/playlists`, {
+      headers: {
+        Authorization: ` Bearer ${this.state.accessToken}`
+      },
+    })
+    .then(response => 
+      response.json()
+    )
+    .then(playlists => {
+      this.setState({	playlists: playlists }, () => {
+        console.log(this.state.playlists);
       });
-    }
+    });
   }
 
   getUserPlaylists = () => {
@@ -81,7 +81,7 @@ class Home extends Component {
         <button onClick={() => {this.getArtist()}}>Artist</button>
         <button onClick={() => {console.log(this.state.me)}}>Me</button>
         <button onClick={() => {console.log(this.state.me.id)}}>Me.id</button>
-        <button onClick={() => {console.log(this.state.playlists)}}>Playlist</button>
+        <button onClick={() => {this.getMyPlaylists()}}>Playlist</button>
       </div>
     );
   } 
